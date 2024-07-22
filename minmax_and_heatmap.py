6ccb933149496ac7522acc6bb4b3376620a05ec3
Adarsha M S <117698505+adarshpatelmulluru@users.noreply.github.com>
2024-07-23 00:55:59 +0530

    Create minmax_and_heatmap.py

---
+++ minmax_and_heatmap.py
@@ -0,0 +1,39 @@
+def minmax(depth,node_index,maximizing_agent,values,path):
+    if depth==3:
+        return values[node_index],path+[node_index]
+    
+    if maximizing_agent:
+        best = float("-inf")
+        best_path=[]
+        value,new_path = minmax(depth+1,node_index*2+1,False,values,path+[node_index])
+        if value > best:
+            best = value
+            best_path = new_path
+        return best,best_path
+    
+    else:
+        best = float("inf")
+        best_path=[]
+        value,new_path = minmax(depth+1,node_index*2+1,True,values,path+[node_index])
+        if value < best:
+            best = value
+            best_path = new_path
+        return best,best_path
+    
+values = [2,5,6,1,5,7,8,12,25]
+
+optimal_value,optimal_path = minmax(0,0,True,values,[])
+print(f"optimal value is {optimal_value}")
+print(f"optimal path is {optimal_path}")
+
+#heat map
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_csv(r'../../datasets/iris.csv')
+dataframe.drop('species',axis=1,inplace=True)
+correlation_matrix = dataframe.corr()
+sns.heatmap(correlation_matrix,cmap="viridis",annot=True)
+plt.show()
